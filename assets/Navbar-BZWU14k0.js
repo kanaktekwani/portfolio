6779c97f4e46c6c5f54e4d7c8ea1402f25d3be27
Kanak Tekwani <kptekwan@asu.edu>
2026-05-23 03:46:16 -0700

    Deploy portfolio site

--- assets/Navbar-BZWU14k0.js
+++ assets/Navbar-BZWU14k0.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-CsXk87YK.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-YqYslxhs.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
